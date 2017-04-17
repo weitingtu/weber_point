a3379844c7695af7ab92f996f500fcb558b724b1
Wei-Ting Tu <bluesoul@ms17.hinet.net>
2017-04-17 23:25:11 +0800

    Use fermat point to decompose

--- weber_point/decomposition.cpp
+++ weber_point/decomposition.cpp
@@ -27,11 +27,27 @@ void Decomposition::decompose()
 {
     _centers.clear();
 
-    for(int i = 0; i < _neighbors.size(); ++i)
+//    for(int i = 0; i < _neighbors.size(); ++i)
+//    {
+//        QPointF p = _get_intersection_point(_poly, _neighbors[i]);
+//        double x = (p.x() - _poly.center.x()) / 3 * 2 + _poly.center.x();
+//        double y = (p.y() - _poly.center.y()) / 3 * 2 + _poly.center.y();
+//        _centers.push_back(QPointF(x, y));
+//    }
+    for(int i = 0; i < _poly.points.size(); ++i)
     {
-        QPointF p = _get_intersection_point(_poly, _neighbors[i]);
-        double x = (p.x() - _poly.center.x()) / 3 + _poly.center.x();
-        double y = (p.y() - _poly.center.y()) / 3 + _poly.center.y();
-        _centers.push_back(QPointF(x, y));
+        Poly p;
+        p.points.push_back(_poly.center);
+        p.points.push_back(_poly.points[i]);
+        if(i == _poly.points.size() - 1)
+        {
+            p.points.push_back(_poly.points[0]);
+        }
+        else
+        {
+            p.points.push_back(_poly.points[i + 1]);
+        }
+        p.center = QPointF((p.points[0].x() + p.points[1].x() + p.points[2].x()) / 3, (p.points[0].y() + p.points[1].y() + p.points[2].y()) / 3);
+        _centers.push_back(p.center);
     }
 }
